9b5f57f0a612538701e0535889b39a42a87fe8d2
notVlad1997 <92084641+notVlad1997@users.noreply.github.com>
2023-12-09 15:27:23 +0200

    First commit
    I've added a first functionality of the app when it presses a button, it will add a button to check if everything works properly with the idea.

---
+++ toolkit_structura_aplicatii_licenta/main.py
@@ -0,0 +1,24 @@
+import tkinter
+from tkinter import ttk
+
+
+class App(tkinter.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
+
+        self.button = ttk.Button(self, text="Create button!")
+        self.button.grid(column=0, row=0)
+        self.pack()
+
+        self.button.bind('<Button-1>', self.create_button)
+
+    def create_button(self, event):
+        self.button1 = ttk.Button(self, text="Created button!")
+        self.button1.grid(column=0, row=1)
+        self.pack()
+
+
+root = tkinter.Tk()
+myapp = App(root)
+myapp.mainloop()
